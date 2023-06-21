866b1957587dc2d72e865f85b4aca41df9e0637f
Damian <rootyniarz@gmail.com>
2023-06-21 22:05:48 +0200

    12. Polecenie 4 cz.3 under construction

--- src/test/java/pl/zajavka/integration/CustomerServiceTest.java
+++ src/test/java/pl/zajavka/integration/CustomerServiceTest.java
@@ -8,11 +8,11 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
 import pl.zajavka.business.*;
-import pl.zajavka.domain.Customer;
-import pl.zajavka.domain.Producer;
-import pl.zajavka.domain.Product;
-import pl.zajavka.domain.StoreFixtures;
+import pl.zajavka.domain.*;
 import pl.zajavka.infrastructure.configuration.ApplicationConfiguration;
+
+import java.time.LocalDate;
+import java.util.List;
 
 @SpringJUnitConfig(classes = ApplicationConfiguration.class)
 @AllArgsConstructor(onConstructor = @__(@Autowired))
@@ -81,7 +81,38 @@ public class CustomerServiceTest {
 
     @Test
     @DisplayName("Polecenie 4 cz.2")
-    void thatPurchaseIsNotRemovedWhenCustomerRemovingFails() {
+    void thatPurchaseAndOpinionIsNotRemovedWhenCustomerRemovingFails() {
+        //given
+        final Customer customer = customerService.create(StoreFixtures.someCustomer()
+                .withDateOfBirth(LocalDate.of(1950,10,4)));
+        final Producer producer = producerService.create(StoreFixtures.someProducer());
+        final Product product1 = productService.create(StoreFixtures.someProduct1(producer));
+        final Product product2 = productService.create(StoreFixtures.someProduct2(producer));
+        Purchase purchase1 = purchaseService.create(StoreFixtures.somePurchase(customer, product1).withQuantity(1));
+        Purchase purchase2 = purchaseService.create(StoreFixtures.somePurchase(customer, product2).withQuantity(3));
+        Opinion opinion1 = opinionService.create(StoreFixtures.someOpinion(customer, product1));
+
+        Assertions.assertEquals(customer, customerService.find(customer.getEmail()));
+
+        //when
+        RuntimeException exception = Assertions
+                .assertThrows(RuntimeException.class,
+                        () -> customerService.remove(customer.getEmail()));
+        Assertions.assertEquals
+                ("Could not remove customer because he/she is older then 40, email: [%s]"
+                        .formatted(customer.getEmail()), exception.getMessage());
+
+        Assertions.assertEquals(customer, customerService.find(customer.getEmail()));
+        Assertions.assertEquals(
+                purchaseService.findAll(customer.getEmail()),
+                List.of(
+
+                )
+                );
+        Assertions.assertEquals(
+                opinionService.findAll(customer.getEmail()),
+
+                );
 
     }
 
